e214a27d31c758a292d44ad0ccd2489b02743682
Nabin kutu <nabin.kutu11@gmail.com>
2025-09-22 16:32:36 +0545

    Add .gitignore file and update requestBody type in API interface

--- src/types/api.ts
+++ src/types/api.ts
@@ -31,7 +31,7 @@ export interface Endpoint {
   category: string;
   parameters?: Parameter[];
   pathParams?: Parameter[];
-  requestBody?: any;
+  requestBody?: SchemaObject;
   responseExamples?: ResponseExample[];
 }
 
